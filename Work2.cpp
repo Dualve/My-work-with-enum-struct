63aa24a68187d68b4026533e9d0a2dceb610535f
Alexander <45214837+Dualve@users.noreply.github.com>
2019-02-19 18:48:34 +0300

    Add files via upload
    It is the replacment of the 1st version

--- Work2.cpp
+++ Work2.cpp
@@ -3,40 +3,44 @@
 using namespace std;
 
 const int amount = 5;
-int choice,number,counter = 0,choicer = 0;
+int choice, number, counter = 0;
 enum district { central, pervomaiskiy, lenenskiy, oktaborskiy };
 district region;
 int rg = region;
 
+
 int main()
 {
 	setlocale(LC_ALL, "Rus");
+	system("color F0");
 
 	struct room
 	{
 		int square;
 		int neighbors;
-		char free;
+		bool free;
+		char type[7];
 	};
 
 	struct flat
 	{
-		char region[20];
+		char region[14];
 		int rooms;
 		int square;
 		int floor;
+		char type[7];
 	};
 
 	struct house
 	{
-		char region[20];
+		char region[14];
 		int building_square;
 		int living_square;
 		int percent_of_ready;
-
+		char type[7];
 	};
 
-	struct accommodation
+	union accommodation
 	{
 		house houses;
 		flat flats;
@@ -45,126 +49,155 @@ int main()
 
 	accommodation acc[amount];
 
-	while(true)
+	while (true)
 	{
-		cout << "Выберите пункт меню:\n" << "1 - добавить запись в массив.\n" << "2 - удаление записи из массива.\n" << "3 - вывод данных массива.\n" <<"4 - выход." << endl;
+		cout << "Выберите пункт меню:\n" << "1 - добавить запись в массив.\n" << "2 - удаление записи из массива.\n" << "3 - вывод данных массива.\n" << "4 - выход." << endl;
 		cin >> choice;
-		if(choice == 1)
+		if (choice == 1)
 		{
-			if(counter < 5)
+			if (counter < 5)
 			{
-				cout << "Введите количество комнат в квартире:" << endl;
-				cin >> acc[counter].flats.rooms;
-				cout << "Введите площадь квартиры:" << endl;
-				cin >> acc[counter].flats.square;
-				cout << "На каком этаже находится квартира:" << endl;
-				cin >> acc[counter].flats.floor;
-				cout << "Выберите район:\n" << "0 - central, 1 - pervomaiskiy, 2 - lenenskiy, 3 - oktaborskiy" << endl;
-				cin >> rg;
-				switch (rg)
+				cout << "Выберите пункт меню 1 - квартира , 2 - комната , 3 - дом\n" << endl;
+				cin >> choice;
+				if (choice - 1 == 0)
 				{
-				case(central):
-					cout << "Выбран Центральный район." << endl;
-					strcpy_s(acc[counter].flats.region,"central");
-					break;
-				case(pervomaiskiy):
-					cout << "Выбран Первомайский район." << endl;
-					strcpy_s(acc[counter].flats.region,"pervomaiskiy");
-					break;
-				case(lenenskiy):
-					cout << "Выбран Лененский район." << endl;
-					strcpy_s(acc[counter].flats.region,"lenenskiy");
-					break;
-				case(oktaborskiy):
-					cout << "Выбран Октябрьский район." << endl;
-					strcpy_s(acc[counter].flats.region,"oktaborskiy");
-					break;
-				default:
-					cout << "Вы ошиблись районом - вам дали жильё в Деревне." << endl;
-					strcpy_s(acc[counter].flats.region, "country");
-				
+					strcpy_s(acc[counter].flats.type, "Flat");
+					cout << "Введите количество комнат в квартире:" << endl;
+					cin >> acc[counter].flats.rooms;
+					cout << "Введите площадь квартиры:" << endl;
+					cin >> acc[counter].flats.square;
+					cout << "На каком этаже находится квартира:" << endl;
+					cin >> acc[counter].flats.floor;
+					cout << "Выберите район:\n" << "0 - central, 1 - pervomaiskiy, 2 - lenenskiy, 3 - oktaborskiy" << endl;
+					cin >> rg;
+					switch (rg)
+					{
+					case(central):
+						cout << "Выбран Центральный район." << endl;
+						strcpy_s(acc[counter].flats.region, "central");
+						break;
+					case(pervomaiskiy):
+						cout << "Выбран Первомайский район." << endl;
+						strcpy_s(acc[counter].flats.region, "pervomaiskiy");
+						break;
+					case(lenenskiy):
+						cout << "Выбран Лененский район." << endl;
+						strcpy_s(acc[counter].flats.region, "lenenskiy");
+						break;
+					case(oktaborskiy):
+						cout << "Выбран Октябрьский район." << endl;
+						strcpy_s(acc[counter].flats.region, "oktaborskiy");
+						break;
+					default:
+						cout << "Вы ошиблись районом - вам дали жильё в Деревне." << endl;
+						strcpy_s(acc[counter].flats.region, "country");
+					}
 				}
-				cout << "Введите площадь комнаты:" << endl;
-				cin >> acc[counter].rooms.square;
-				cout << "Сколько соседей у вас:" << endl;
-				cin >> acc[counter].rooms.neighbors;
-				cout << "Комната свободна (y - yes/n - no):" << endl;
-				cin >> acc[counter].rooms.free;
-				cout << "Какова площадь дома:" << endl;
-				cin >> acc[counter].houses.building_square;
-				cout << "Какова жилая площадь дома:" << endl;
-				cin >> acc[counter].houses.living_square;
-				cout << "На сколько % готов дом:" << endl;
-				cin >> acc[counter].houses.percent_of_ready;
-				cout << "Выберите район:\n" << "0 - central, 1 - pervomaiskiy, 2 - lenenskiy, 3 - oktaborskiy" << endl;
-				cin >> rg;
-				switch (rg)
+				else if (choice - 1 == 1)
 				{
-				case(central):
-					cout << "Выбран Центральный район." << endl;
-					strcpy_s(acc[counter].houses.region, "central");
-					break;
-				case(pervomaiskiy):
-					cout << "Выбран Первомайский район." << endl;
-					strcpy_s(acc[counter].houses.region, "pervomaiskiy");
-					break;
-				case(lenenskiy):
-					cout << "Выбран Лененский район." << endl;
-					strcpy_s(acc[counter].houses.region, "lenenskiy");
-					break;
-				case(oktaborskiy):
-					cout << "Выбран Октябрьский район." << endl;
-					strcpy_s(acc[counter].houses.region, "oktaborskiy");
-					break;
-				default:
-					cout << "Вы ошиблись районом - вам выдали жильё в Деревне." << endl;
-					strcpy_s(acc[counter].houses.region, "country");
-					break;
+					strcpy_s(acc[counter].rooms.type, "Room");
+					cout << "Введите площадь комнаты:" << endl;
+					cin >> acc[counter].rooms.square;
+					cout << "Сколько соседей у вас:" << endl;
+					cin >> acc[counter].rooms.neighbors;
+					cout << "Комната свободна (1 - yes/other - no):" << endl;
+					cin >> choice;
+					if (choice == 1)
+					{
+						acc[counter].rooms.free = true;
+					}
+					else
+					{
+						acc[counter].rooms.free = false;
+					}
+				}
+				else
+				{
+					strcpy_s(acc[counter].houses.type, "House");
+					cout << "Какова площадь дома:" << endl;
+					cin >> acc[counter].houses.building_square;
+					cout << "Какова жилая площадь дома:" << endl;
+					cin >> acc[counter].houses.living_square;
+					cout << "На сколько % готов дом:" << endl;
+					cin >> acc[counter].houses.percent_of_ready;
+					cout << "Выберите район:\n" << "0 - central, 1 - pervomaiskiy, 2 - lenenskiy, 3 - oktaborskiy" << endl;
+					cin >> rg;
+					switch (rg)
+					{
+					case(central):
+						cout << "Выбран Центральный район." << endl;
+						strcpy_s(acc[counter].houses.region, "central");
+						break;
+					case(pervomaiskiy):
+						cout << "Выбран Первомайский район." << endl;
+						strcpy_s(acc[counter].houses.region, "pervomaiskiy");
+						break;
+					case(lenenskiy):
+						cout << "Выбран Лененский район." << endl;
+						strcpy_s(acc[counter].houses.region, "lenenskiy");
+						break;
+					case(oktaborskiy):
+						cout << "Выбран Октябрьский район." << endl;
+						strcpy_s(acc[counter].houses.region, "oktaborskiy");
+						break;
+					default:
+						cout << "Вы ошиблись районом - вам выдали жильё в Деревне." << endl;
+						strcpy_s(acc[counter].houses.region, "country");
+						break;
+					}
 				}
 				counter++;
 			}
 			else
 			{
-				cout << "Массив переполнен. Удалите 1 элемент." << endl;
-			}
-			
-
-		}
-		else if(choice == 2)
-		{
-			
-			cout << "Какую запись из массива структур будем удалять?(0/1/2/3/4) " << endl;
-			cin >> number;
-
-
-			for(int j=number; j < amount ;j++)
-			{
-
-				acc[j] = acc[j + 1];
-	
+				cout << "\nМассив переполнен. Удалите 1 элемент.\n" << endl;
 			}
 
-			counter--;
-		
+
 		}
-		else if(choice == 3)
+		else if (choice == 2)
 		{
-			
-			for(int i=0; i < counter; i++)
+			if (counter > 0)
 			{
-				cout << "\n" << i+1 << ")" << endl;
-				cout << acc[i].rooms.square << " - площадь комнаты, " << acc[i].rooms.neighbors << " - количество соседей, " << acc[i].rooms.free << " - свободна ли(n - нет/y - да)." << endl;
-				cout << acc[i].flats.square << " - площадь квартиры, " << acc[i].flats.rooms << " - количество комнат, " << acc[i].flats.floor << " - этаж, " << acc[i].flats.region << " - район." << endl;
-				cout << acc[i].houses.building_square << " - площадь здания, " << acc[i].houses.living_square << " - жилая площадь, " << acc[i].houses.percent_of_ready << " - процент готовности, " << acc[i].houses.region << " - район.\n" <<  endl;
+				cout << "Какую запись из массива структур будем удалять? ( 1 -  " << counter << " )" << endl;
+				cin >> number;
+
+				for (int j = number - 1; j < amount; j++)
+				{
+					acc[j] = acc[j + 1];
+				}
+
+				counter--;
+			}
+			else
+			{
+				cout << "\nМассив пуст, добавьте элементы.\n" << endl;
+			}
+		}
+		else if (choice == 3)
+		{
+			for (int i = 0; i < counter; i++)
+			{
+				cout << "\n" << i + 1 << ") ";
+				if (!strcmp(acc[i].rooms.type, "Room"))
+				{
+					cout << acc[i].rooms.square << " - площадь комнаты, " << acc[i].rooms.neighbors << " - количество соседей. "; acc[i].rooms.free == true ? cout << " Комната свободна. " << endl : cout << "Комната занята. " << endl;
+				}
+				else if (!strcmp(acc[i].flats.type, "Flat"))
+				{
+					cout << acc[i].flats.square << " - площадь квартиры, " << acc[i].flats.rooms << " - количество комнат, " << acc[i].flats.floor << " - этаж, " << acc[i].flats.region << " - район." << endl;
+				}
+				else if (!strcmp(acc[i].flats.type, "House"))
+				{
+					cout << acc[i].houses.building_square << " - площадь здания, " << acc[i].houses.living_square << " - жилая площадь, " << acc[i].houses.percent_of_ready << " - процент готовности, " << acc[i].houses.region << " - район.\n" << endl;
+				}
 			}
 
 		}
 		else if (choice == 4)
 		{
-
 			cout << "Удачи." << endl;
 			break;
-			
 		}
 		else
 		{
@@ -174,5 +207,4 @@ int main()
 
 	system("Pause");
 	return 0;
-
 }
